0366cbc2f59c4d9f1d88f94c88433df0f90ef0e2
Tristram Reed <tar123@txstate.edu>
2023-10-03 14:36:37 -0500

    thread routes

--- express-app/index.js
+++ express-app/index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose)
 const cors = require('cors');
 const CONNECTION_URL = 'mongodb://127.0.0.1:27017/forum_app_db';
 
 var testRoutes = require('./routes/testAPI');
+var threadRoutes = require('./routes/threadRoute');
 
 const app = express()
 const PORT = 8080
 app.use(cors());
 
+app.use('/thread', threadRoutes);
 app.use('/testAPI', testRoutes);
 app.get("/", (req, res) => {
   res.send("Hello World!");
