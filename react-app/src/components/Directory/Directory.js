2f9525b9a766d356c8d368ccdb29b147a0bc6a9d
Vinson Martin <vm84039@yahoo.com>
2023-10-03 14:14:46 -0700

    Moved Thread to directory page

--- react-app/src/components/Directory/Directory.js
+++ react-app/src/components/Directory/Directory.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Thread from "../Thread/Thread";
+import ThreadHeader from "../Thread/ThreadHeader";
+import Logout from "../Logout";
+import Container from "react-bootstrap/esm/Container";
 import "./Directory.css"
 
 const Directory = () => {
   return (
     <div className="directory-container">
-
+      <Logout />
+      <Container className="forum-container">
+        <ThreadHeader />
+        <Thread />
+      </Container>
     </div>
   )
 }
