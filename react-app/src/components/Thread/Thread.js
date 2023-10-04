2cd7a9fa1cd8102096ceeb30acfe0b90aaa2dda8
Vinson Martin <vm84039@yahoo.com>
2023-10-04 09:24:11 -0700

    Thread Api

--- react-app/src/components/Thread/Thread.js
+++ react-app/src/components/Thread/Thread.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Accordion from "react-bootstrap/Accordion";
 import { Container, Button } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
@@ -7,11 +7,23 @@ import Collapse from "react-bootstrap/Collapse";
 import Replies from "./Replies";
 import { formatDate, formatTime } from "../../service/DateTimeFormatter";
 import "./Thread.css";
+import { getAllThreads } from "../../service/ThreadAPI";
+
 
 const Thread = () => {
   const sampleDate = "2023-10-02";
   const sampleTime = "14:30:00";
   const [open, setOpen] = useState(false);
+  const [rowData, setRowData] = useState([]);
+
+
+  useEffect(() => {
+    getAllThreads()
+    .then((data) => setRowData(data)) 
+    .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+
 
   return (
     <div className="thread-container">
@@ -22,47 +34,52 @@ const Thread = () => {
             <Container fluid className="child">
               <Row className="rowHeader">
                 <Col md={1} className="col1">
-                  #
+                  Author
+                </Col>
+                <Col md={2} className="col2">
+                  Title
                 </Col>
                 <Col md={7} className="col2">
                   Thread
                 </Col>
-                <Col md={2} className="col3">
-                  Last Post
-                </Col>
+
                 <Col md={1} className="col4">
                   Replies
                 </Col>
                 <Col md={1} className="col4"></Col>
               </Row>
+              {rowData.map((thread) => (
+                <>
               <Row className="rowThread">
-                <Col md={1}>1</Col>
-                <Col md={7}>Sample thread</Col>
-                <Col md={2}>
-                  {" "}
-                  {formatDate(sampleDate)}
-                  <br />
-                  {formatTime(sampleTime)}
-                </Col>
+                <Col md={1}>{thread.author}</Col>
+                <Col md={2}>{thread.title}</Col>
+                <Col md={7}>{thread.content}</Col>
                 <Col md={1}>1</Col>
                 <Col md={1}>
                   <Button
                     onClick={() => setOpen(!open)}
-                    aria-controls="replies-collapse-text"
+                    aria-controls={thread._id}
                     aria-expanded={open}
                     className="btn-link no-hover"
+                    key={thread._id}
                   >
                     View
                   </Button>
                 </Col>
               </Row>
+                       
+
               <Row>
                 <Collapse in={open}>
-                  <div id="replies-collapse-text">
-                    <Replies />
+                  <div id={thread._id}>
+                    <Replies 
+                      replies={thread.replies} 
+                      key={thread._id}
+                    />
                   </div>
                 </Collapse>
-              </Row>
+              </Row></>
+               ))}
             </Container>
           </Accordion.Body>
         </Accordion.Item>
