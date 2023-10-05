7d392bbb290a02261db48cc02a4aad28714afe38
Tristram Reed <tar123@txstate.edu>
2023-10-05 13:28:35 -0500

    handle logout

--- react-app/src/components/Directory/Directory.js
+++ react-app/src/components/Directory/Directory.js
@@ -4,16 +4,39 @@ import ThreadHeader from "../Thread/ThreadHeader";
 import Logout from "../Logout";
 import Container from "react-bootstrap/esm/Container";
 import "./Directory.css"
+import {useAuth} from "../../service/AuthContextProvider";
 
 const Directory = () => {
-  return (
-    <div className="directory-container">
+  const {isLoggedIn} = useAuth();
+
+  function renderNotLoggedIn() {
+    return (
+      <div className="lander">
+        <h1>Cannot Load Threads</h1>
+        <p className="text-muted">A General Discussion Site</p>
+        <br/>
+        <br/>
+        <p>Please Login to access the discussion!</p>
+      </div>
+    );
+  }
+
+  function renderDirectory() {
+    return(
+      <div className="directory-container">
       <Logout />
       <Container className="forum-container">
         <ThreadHeader />
         <Thread />
       </Container>
     </div>
+    )
+  }
+
+  return (
+    <>
+      {isLoggedIn ? renderDirectory() : renderNotLoggedIn()}
+    </>
   )
 }
 
--- react-app/src/components/Logout.js
+++ react-app/src/components/Logout.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Container, Button } from "react-bootstrap";
+import { useAuth } from "../service/AuthContextProvider";
 
 
 const Logout = () => {
+  const {logout } = useAuth();
     const navigate = useNavigate(); 
+
+    function handleLogout() {
+      logout();
+      navigate("/")
+    }
 
   return (
     <Container fluid className="d-flex justify-content-end logout">
-      <Button className="btn btn-danger mx-2" onClick={() => navigate("/")}>
+      <Button className="btn btn-danger mx-2" onClick={() => handleLogout()}>
         <div className="logout-icon">Logout</div>
       </Button>
     </Container>
